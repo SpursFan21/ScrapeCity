2ce32ffff72d9da86e46ddfaadfa28c6205cdb67
SpursFan21 <duncanfrederic@gmail.com>
2024-10-09 21:27:28 +1300

    update variable from int to uuid for order_id

--- backend/backend/scraping_engine/urls.py
+++ backend/backend/scraping_engine/urls.py
@@ -6,5 +6,5 @@ from .views import scrape_view, ScrapingOrdersList, ScrapingOrderDetail
 urlpatterns = [
     path('scrape/', scrape_view, name='scrape'),
     path('api/scraping-orders/', ScrapingOrdersList.as_view(), name='scraping-orders'),
-    path('api/scraping-order/<int:order_id>/', ScrapingOrderDetail.as_view(), name='scraping-order-detail'),
+    path('api/scraping-order/<uuid:order_id>/', ScrapingOrderDetail.as_view(), name='scraping-order-detail'),
 ]
